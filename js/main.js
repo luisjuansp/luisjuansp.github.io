35a5afb2ea2cde97f15ab5dea4c8ffc777c9a67e
Luis Juan Sanchez <luisjuansp@gmail.com>
2016-10-07 15:50:54 -0500

    materialize

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Initialize collapse button
+$(".button-collapse").sideNav({closeOnClick: true});
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+// $('.collapsible').collapsible();
+
+$(document).ready(function(){
+	$('.parallax').parallax();
+});
